281daf4f06f7e5767d74b4af76b8d2a8c7440168
Zhiharev <slipdoll@gmail.com>
2021-04-09 16:55:24 +0300

    add users methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,6 +14,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    const IS_ADMIN = 1;
+    const IS_USER = 0;
+
+    const STATUS_BANNED = 1;
+    const STATUS_IS_ACTIVE = 0;
 
     /**
      * The attributes that are mass assignable.
@@ -60,5 +69,123 @@ class User extends Authenticatable
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * @param array $fields
+     * @return static
+     */
+    public static function add(array $fields): self
+    {
+        $user = new static;
+
+        $user->fill($fields);
+        $user->password = bcrypt($fields['password']);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit(array $fields): void
+    {
+        $this->fill($fields);
+        $this->password = bcrypt($fields['password']);
+        $this->save();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function remove(): void
+    {
+        $this->delete();
+    }
+
+    /**
+     * @param UploadedFile $avatar
+     */
+    public function uploadAvatar(UploadedFile $avatar): void
+    {
+        if (!$avatar) {
+            return;
+        }
+
+        Storage::delete('uploads/'.$this->image);
+        $fileName = Str::random(10) . '.' . $avatar->extension();
+        $avatar->storeAs('uploads', $fileName);
+        $this->image = $fileName;
+        $this->save();
+    }
+
+    /**
+     * @return string
+     */
+    public function getAvatar(): string
+    {
+        if ($this->image) {
+            return '/img/no-user-image.png';
+        }
+
+        return '/uploads/' . $this->image;
+    }
+
+    /**
+     *
+     */
+    public function makeAdmin(): void
+    {
+        $this->update(['is_admin' => static::IS_ADMIN]);
+    }
+
+    /**
+     *
+     */
+    public function makeStandardUser(): void
+    {
+        $this->update(['is_admin' => static::IS_USER]);
+    }
+
+    /**
+     * @param bool|null $value
+     */
+    public function toggleAdmin(?bool $value): void
+    {
+        if (!$value) {
+            $this->makeStandardUser();
+
+            return;
+        }
+
+        $this->makeAdmin();
+    }
+
+    /**
+     *
+     */
+    public function ban(): void
+    {
+        $this->update(['status' => static::STATUS_BANNED]);
+    }
+
+    /**
+     *
+     */
+    public function unban(): void
+    {
+        $this->update(['status' => static::STATUS_IS_ACTIVE]);
+    }
+
+    /**
+     * @param bool|null $value
+     */
+    public function toggleBan(?bool $value): void
+    {
+        if (!$value) {
+            $this->unban();
+
+            return;
+        }
+
+        $this->ban();
     }
 }
