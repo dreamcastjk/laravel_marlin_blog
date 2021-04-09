4792222749b027ceb41e278f8d0f89275e3aa57b
Zhiharev <slipdoll@gmail.com>
2021-04-09 17:30:06 +0300

    add comment methods

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,6 +10,9 @@ class Comment extends Model
 {
     use HasFactory;
 
+    const STATUS_NOT_ACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+
     /**
      * Post that have this comment.
      *
@@ -28,5 +31,40 @@ class Comment extends Model
     public function author(): HasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    public function allow(): void
+    {
+        $this->update(['status' => static::STATUS_ACTIVE]);
+    }
+
+    public function disallow(): void
+    {
+        $this->update(['status' => static::STATUS_NOT_ACTIVE]);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function toggleStatus(): void
+    {
+        switch ($this->status) {
+            case static::STATUS_NOT_ACTIVE:
+                $this->allow();
+                break;
+            case static::STATUS_ACTIVE:
+                $this->disallow();
+                break;
+            default:
+                throw new \Exception('Not valid status.');
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function remove(): void
+    {
+        $this->delete();
     }
 }
--- database/migrations/2021_04_08_145557_create_comments_table.php
+++ database/migrations/2021_04_08_145557_create_comments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Comment;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,7 @@ class CreateCommentsTable extends Migration
             $table->text('text');
             $table->integer('user_id');
             $table->integer('post_id');
+            $table->integer('status')->default(Comment::STATUS_NOT_ACTIVE);
             $table->timestamps();
         });
     }
