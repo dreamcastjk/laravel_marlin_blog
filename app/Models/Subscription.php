4ecb70dfdd7c581e1931b17a40b471e7cf116028
Zhiharev <slipdoll@gmail.com>
2021-04-09 17:40:31 +0300

    add subscription methods

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Subscription extends Model
 {
     use HasFactory;
+
+    /**
+     * @param string $email
+     * @return static
+     */
+    public static function add(string $email): self
+    {
+        $sub = new static;
+        $sub->email = $email;
+        $sub->token = Str::random(100);
+        $sub->save();
+
+        return $sub;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function remove(): void
+    {
+        $this->delete();
+    }
 }
