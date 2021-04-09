53aa74e0156c3f41a3f236e3c4f26ce48ac51615
Zhiharev <slipdoll@gmail.com>
2021-04-09 13:55:04 +0300

    add migrations and models

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    const STATUS_DRAFT = 0;  // черновик
+}
