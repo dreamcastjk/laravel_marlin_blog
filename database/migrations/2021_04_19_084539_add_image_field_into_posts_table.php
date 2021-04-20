804046c4d0ec1b15fb303b220e0cab633bafa200
Zhiharev <slipdoll@gmail.com>
2021-04-20 16:10:01 +0300

    partial posts crud logic

---
+++ database/migrations/2021_04_19_084539_add_image_field_into_posts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImageFieldIntoPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', fn (Blueprint $table) => $table->string('image')->nullable());
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', fn (Blueprint $table) => $table->dropColumn('image'));
+    }
+}
