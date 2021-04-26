9623a293fe83d5e395d0e09fdcf459bbc8228684
Zhiharev <slipdoll@gmail.com>
2021-04-26 11:50:28 +0300

    front end layout, main page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\TagsController;
 use App\Http\Controllers\Admin\PostsController;
 use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->group(function () {
     Route::get('', [DashboardController::class, 'index'])->name('dashboard');
@@ -20,3 +19,7 @@ Route::prefix('admin')->group(function () {
 });
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
